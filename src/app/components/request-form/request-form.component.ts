7cd1daaa424fe39d746aa9379f59e1ea9add8b30
SamiChafik <sami.chafik00@gmail.com>
2025-06-22 23:25:17 +0100

    request form

--- src/app/components/request-form/request-form.component.ts
+++ src/app/components/request-form/request-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { RequestService } from '../../services/request.service';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-request-form',
@@ -22,46 +23,63 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatDialogModule,
     MatSnackBarModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule
   ],
 })
-export class RequestFormComponent {
+export class RequestFormComponent implements OnInit {
   requestForm: FormGroup;
+  isEditMode = false;
+  statusOptions = ['WAITING', 'ACCEPTED', 'REJECTED'];
 
   constructor(
     private fb: FormBuilder,
     private requestService: RequestService,
     private snackBar: MatSnackBar,
     public dialogRef: MatDialogRef<RequestFormComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { announcementId: number }
+    @Inject(MAT_DIALOG_DATA) public data: {
+      announcementId: number,
+      request?: any
+    }
   ) {
     this.requestForm = this.fb.group({
       dimensions: ['', Validators.required],
-      weight: ['', Validators.required],
-      type: ['', Validators.required],
-      status:['WAITING']
+      weight: ['', [Validators.required, Validators.min(0.1)]],
+      type: ['', Validators.required]
     });
+  }
+
+  ngOnInit(): void {
+    if (this.data.request) {
+      this.isEditMode = true;
+      this.requestForm.patchValue(this.data.request);
+    }
   }
 
   onSubmit(): void {
     if (this.requestForm.valid) {
-      this.requestService
-        .createRequest({
-          ...this.requestForm.value,
-          announcementId: this.data.announcementId,
-        })
-        .subscribe({
-          next: () => {
-            this.snackBar.open('Transport request submitted!', 'Close', {
-              duration: 3000,
-            });
-            this.dialogRef.close(true);
-          },
-          error: () =>
-            this.snackBar.open('Error submitting request', 'Close', {
-              duration: 3000,
-            }),
-        });
+      const formData = {
+        ...this.requestForm.value,
+        announcementId: this.data.announcementId
+      };
+
+      const operation = this.isEditMode
+        ? this.requestService.updateRequest(this.data.request.id, formData)
+        : this.requestService.createRequest(formData);
+
+      operation.subscribe({
+        next: () => {
+          this.snackBar.open(`Request ${this.isEditMode ? 'updated' : 'created'} successfully!`, 'Close', {
+            duration: 3000,
+          });
+          this.dialogRef.close(true);
+        },
+        error: () => {
+          this.snackBar.open(`Error ${this.isEditMode ? 'updating' : 'creating'} request`, 'Close', {
+            duration: 3000,
+          });
+        }
+      });
     }
   }
 }
