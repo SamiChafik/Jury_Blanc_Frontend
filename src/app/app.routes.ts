ea3e76103c8fe562f4e758fd13e0d9472002d585
SamiChafik <sami.chafik00@gmail.com>
2025-06-22 23:26:46 +0100

    new paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AnnouncementListComponent } from './components/announcement-list/announcement-list.component';
+import { RequestListComponent } from './components/request-list/request-list.component';
+import { AuthGuard } from './guard/auth.guard';
+import { AdminGuard } from './guard/admin.guard';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 export const routes: Routes = [
+  { path: 'announcement', component: AnnouncementListComponent, canActivate:[AuthGuard]},
+  { path: 'requestlist', component: RequestListComponent, canActivate:[AuthGuard] },
+  { path: 'users', component: UserListComponent, canActivate:[AuthGuard, AdminGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent }
 ];
