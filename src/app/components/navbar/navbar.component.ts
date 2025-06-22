bf793d175769fd6e08705434539d55c906d42b62
SamiChafik <sami.chafik00@gmail.com>
2025-06-22 23:24:05 +0100

    modifications to the navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-navbar',
@@ -14,17 +18,31 @@ import { MatSnackBar } from '@angular/material/snack-bar';
     CommonModule,
     MatButtonModule,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatMenuModule
   ]
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   isMenuCollapsed = true;
+  currentUser: any = null;
 
   constructor(
       public authService: AuthService,
       private router: Router,
       private snackBar: MatSnackBar
     ) {}
+
+    ngOnInit(): void {
+    this.currentUser = this.authService.getUser();
+
+    if (this.authService.userUpdated$) {
+      this.authService.userUpdated$.subscribe(() => {
+        this.currentUser = this.authService.getUser();
+      });
+    }
+  }
+
+
 
   logout() {
     this.authService.removeAuthData();
