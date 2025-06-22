138874491415931302ed54e4a2530c25e7dec7f1
SamiChafik <sami.chafik00@gmail.com>
2025-06-22 03:41:00 +0100

    request service

---
+++ src/app/services/request.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class RequestService {
+  private apiUrl = 'http://localhost:8080/requests';
+
+  constructor(private http: HttpClient) {}
+
+  createRequest(request: {
+    dimensions: string;
+    weight: string;
+    type: string;
+    status: string;
+    announcementId: number
+  }): Observable<any> {
+    return this.http.post(this.apiUrl, request);
+  }
+
+  getRequests(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  updateRequestStatus(id: number, status: string): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${id}/status`, { status });
+  }
+
+  deleteRequest(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
